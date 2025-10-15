0d32ac8af5d5dc7abfcf7e767e2b1a03541f6119
2Bmodular <evangelistafabio985@gmail.com>
2025-10-14 22:39:45 -0300

    Pegando nome

--- app/src/main/java/com/example/relativelayoutmotiv/UserActivity.kt
+++ app/src/main/java/com/example/relativelayoutmotiv/UserActivity.kt
@@ -1,6 +1,10 @@
 package com.example.relativelayoutmotiv
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,20 @@ class UserActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        var btnSave = findViewById<Button>(R.id.buttonSave);
+
+        btnSave.setOnClickListener{
+            var nomeText = findViewById<EditText>(R.id.editTextNome).text;
+            if (nomeText.isEmpty()){
+                Toast.makeText(this,"Digite seu nome", Toast.LENGTH_LONG).show()
+            } else {
+                var intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
+
+
 }
